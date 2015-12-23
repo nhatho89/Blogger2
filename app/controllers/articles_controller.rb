045d6c3855c5dcd2ece517f24d1dea9bc8f31c0e
Nhat <nhat.ho1989@gmail.com>
2015-12-23 12:59:56 -0600

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,10 @@ class ArticlesController < ApplicationController
   end
 
   def show
+
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
